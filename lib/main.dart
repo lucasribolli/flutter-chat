9ea6e09d2dd48ddc0c526526f60247b140dd807f
lucasribolli <lucasribolli@gmail.com>
2021-08-21 17:05:46 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text('FLutter'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Firebase.initializeApp();
+          FirebaseFirestore firestore = FirebaseFirestore.instance;
+          firestore
+          .collection('chat')
+          .snapshots().listen((querySnapshot) { 
+            querySnapshot.docs.forEach((element) {
+              print(element.data());
+            });
+          });
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
