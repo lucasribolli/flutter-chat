b3fde06a23f63c5bd3cd421f3053e39a9bda2c50
Lucas Ribolli <lucasribolli@gmail.com>
2021-09-22 22:06:38 -0300

    add firebase messaging

--- lib/core/services/notification/chat_notification_service.dart
+++ lib/core/services/notification/chat_notification_service.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:chat/core/models/chat_notification.dart';
@@ -23,6 +24,63 @@ class ChatNotificationService extends ChangeNotifier {
   void remove(int i) {
     _items.removeAt(i);
     notifyListeners();
+  }
+
+  Future<void> init() async {
+    await _configureForeground();
+    await _configureBackground();
+    await _configureTerminated();
+  }
+
+  Future<void> _configureBackground() async {
+    if (await isAuthorized) {
+      FirebaseMessaging.onMessageOpenedApp.listen(_messageHandler);
+    }
+  }
+
+  Future<void> _configureTerminated() async {
+    if (await isAuthorized) {
+      RemoteMessage? initalMessage = await FirebaseMessaging.instance.getInitialMessage();
+      _messageHandler(initalMessage);
+    }
+  }
+
+  Future<void> _configureForeground() async {
+    if (await isAuthorized) {
+      FirebaseMessaging.onMessage.listen(_messageHandler);
+    }
+  }
+
+  Future<bool> get isAuthorized async {
+    final messaging = FirebaseMessaging.instance;
+    final settings = await messaging.requestPermission();
+    return settings.authorizationStatus == AuthorizationStatus.authorized;
+  }
+
+  void _messageHandler(RemoteMessage? message) {
+    if (message == null) {
+      return;
+    }
+    if (message.notification == null) {
+      return;
+    }
+    final notification = message.notification!;
+    if (_notificationAlreadyExists(notification.title!)) {
+      return;
+    }
+    add(ChatNotification(
+      title: notification.title ?? 'Not know',
+      body: notification.body ?? 'Not know',
+    ));
+  }
+
+  bool _notificationAlreadyExists(String title) {
+    for (final item in _items) {
+      if (item.title == title) {
+        return true;
+      }
+    }
+    return false;
   }
 
   @override
